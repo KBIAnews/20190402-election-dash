0d92c31349ad179923d98378712457a739cd2c77
Nathan Lawrence <nathan@nathanlawrence.org>
2019-04-02 11:00:07 -0500

    GSheet parsing. Excellent.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  GoogleSheetsContextConsumer,
+  GoogleSheetsContextProvider
+} from "./components/services/GoogleSheetsContext";
+import GoogleSheetsFetcher from "./components/services/GoogleSheetsFetcher";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GoogleSheetsContextProvider>
+          <GoogleSheetsFetcher
+            sheetURL={
+              "https://docs.google.com/spreadsheets/d/1Z6zn8Tnwne3CgJh3vHnpRa4gHtKGvPHQeIx6bH0D5xU/"
+            }
+          />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <GoogleSheetsContextConsumer>
+            {context => (<>{
+                !context.pageMustSuspend && (
+                    <>
+                    {context.getRaces().map(race => (
+                        <div className="race" key={race.slug}>
+                          <p>{race.name}</p>
+                          <pre style={{wordWrap: "break-word",whiteSpace: "pre-wrap"}}>{JSON.stringify(race)}</pre>
+                        </div>
+                    ))}
+                    </>
+                )
+            }</>)}
+          </GoogleSheetsContextConsumer>
+        </GoogleSheetsContextProvider>
       </div>
     );
   }
---
+++ src/components/services/GoogleSheetsContext.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+export const defaultSheetContext = {};
+
+export const GoogleSheetsContext = React.createContext(defaultSheetContext);
+
+export class GoogleSheetsContextProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageMustSuspend: true,
+      pageShouldRemindScrollable: false,
+      rawWorkbookData: null,
+      rawSheetsData: null,
+      workbookKey: null
+    };
+  }
+
+  setRawSheetsData(data) {
+    this.setState({
+      rawSheetsData: data
+    });
+  }
+
+  setRawWorkbookData(data) {
+    this.setState({
+      rawWorkbookData: data
+    });
+  }
+
+  setWorkbookKey(key) {
+    this.setState({
+      workbookKey: key
+    });
+  }
+
+  removeSuspense() {
+    this.setState({
+      pageMustSuspend: false
+    });
+  }
+
+  imposeSuspense() {
+    this.setState({
+      pageMustSuspend: true
+    });
+  }
+
+  static getValueFromKeyValueSheet(sheet, key) {
+    return sheet.rows.filter(el => {
+      return el.key === key;
+    })[0].value;
+  }
+
+  static elementIsAnnotation(el) {
+    return el.categorySlug.includes("--");
+  }
+
+  getLabel(key) {
+    return GoogleSheetsContextProvider.getValueFromKeyValueSheet(
+      this.state.rawSheetsData.labels,
+      key
+    );
+  }
+
+  getCategoryColor(slug) {
+    return this.state.rawSheetsData.design.rows.filter(el => {
+      return el.slug === slug;
+    })[0].hexcolor;
+  }
+
+  getCategoryName(slug) {
+    return this.state.rawSheetsData.design.rows.filter(el => {
+      return el.slug === slug;
+    })[0].displayname;
+  }
+
+  getCategoriesList() {
+    return this.state.rawSheetsData.design.rows
+      .filter(el => {
+        // Don't give me the
+        return !el.slug.includes("--");
+      })
+      .map(el => {
+        return {
+          name: el.displayname,
+          slug: el.slug,
+          color: el.hexcolor
+        };
+      });
+  }
+
+  getRaces() {
+    console.log("getracestrigger", this.state.rawSheetsData);
+    let races = this.state.rawSheetsData.races.rows.map(row => ({
+      slug: row.slug,
+      name: row.name,
+      county: row.county,
+      totalPrecincts: row.totalPrecincts,
+      reportedPrecincts: this.state.rawSheetsData.data.rows.filter(
+        el => el.raceslug === row.slug && el.candidate === "reportingPrecincts"
+      )[0].value,
+      candidates: this.state.rawSheetsData.data.rows
+        .filter(
+          el =>
+            el.raceslug === row.slug && el.candidate !== "reportingPrecincts"
+        )
+        .map(el => ({
+          name: el.candidate,
+          votes: parseInt(el.value)
+        }))
+    }));
+    return races;
+  }
+
+  render() {
+    return (
+      <GoogleSheetsContext.Provider
+        value={{
+          ...this.state,
+          setRawSheetsData: this.setRawSheetsData.bind(this),
+          setRawWorkbookData: this.setRawWorkbookData.bind(this),
+          setWorkbookKey: this.setWorkbookKey.bind(this),
+          removeSuspense: this.removeSuspense.bind(this),
+          imposeSuspense: this.imposeSuspense.bind(this),
+          getLabel: this.getLabel.bind(this),
+          getCategoryColor: this.getCategoryColor.bind(this),
+          getCategoryName: this.getCategoryName.bind(this),
+          getCategoriesList: this.getCategoriesList.bind(this),
+          getRaces: this.getRaces.bind(this)
+        }}
+      >
+        {this.props.children}
+      </GoogleSheetsContext.Provider>
+    );
+  }
+}
+
+export let GoogleSheetsContextConsumer = GoogleSheetsContext.Consumer;
